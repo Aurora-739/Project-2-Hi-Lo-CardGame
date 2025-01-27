f642fa74518ef4d3faebac70b6e5e485e3d63df2
Aurora Keay <yfifej9y@students.codeinstitute.net>
2025-01-27 12:37:24 +0000

    start on the rules box, add stylings and start js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    const startButton = document.getElementById('start');
+    //Add on click listener for button
+    startButton.addEventListener('click', function() {
+        //Select (h1) heading by id, and then change it's value to (bananas)
+        document.getElementById('rulescountBox').innerText = document.getElementById('rules')
+    })
 
 });
