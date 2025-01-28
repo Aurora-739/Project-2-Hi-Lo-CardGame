e1a0f2a4abc29b0259f080664ae8b6f3e22dcfd3
Aurora Keay <yfifej9y@students.codeinstitute.net>
2025-01-28 12:01:34 +0000

    make card image only show after the deck has been clicked on

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const startButton = document.getElementById('start');
     const howToPlay = document.getElementById('rulescountBox');
 
+    /*MAKE THE RULES BOX AND SCORE VOUNTER VISIBLE WHEN THE START BUTTON IS CLICKED*/
     startButton.addEventListener("click", event => {
         if (howToPlay.style.visibility === "hidden") {
             howToPlay.style.visibility = "visible";
@@ -10,14 +11,21 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    const deck = document.getElementById('deckButton');
+    const deckButton = document.getElementById('deckButton');
     const card = document.getElementById('inPlay');
-    
+/*MAKE THE DECK DEAL A NEW CARD WHEN DECK IS CLICKED*/
     deck.addEventListener("click", event => {
-        img = document.createElement("img")
-        img.src = "/assets/images/King-d.JPG";
-        img.alt = "King of diamonds";
+        if (card.style.visibility = "visible") {
+            let cardList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+            /*CREATES THE IMAGE*/
+            var img = document.createElement("img");
+            img.src = "/assets/images/King-d.JPG";
+            var div = document.getElementById("inPlay");
+            div.appendChild(img);
+        }
     });
+
+    
     
 
 
