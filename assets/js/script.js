05c415dbc1d43bdd34406f8dd3737cc09a0789b9
Aurora Keay <yfifej9y@students.codeinstitute.net>
2025-01-28 11:22:51 +0000

    make the deck a button with an image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const startButton = document.getElementById('start');
     const howToPlay = document.getElementById('rulescountBox');
 
     startButton.addEventListener("click", event => {
-        if(howToPlay.style.visibility === "hidden"){
+        if (howToPlay.style.visibility === "hidden") {
             howToPlay.style.visibility = "visible";
-        }
-        else{
+        } else {
             howToPlay.style.visibility = "hidden";
         }
     });
-        
+
+    const deck = document.getElementById('deckButton');
+    const card = document.getElementById('inPlay');
+    
+    deck.addEventListener("click", event => {
+        img = document.createElement("img")
+        img.src = "/assets/images/King-d.JPG";
+        img.alt = "King of diamonds";
+    });
+    
 
 
 });
